e5d5c272198363c42b3d3bc5ed648e2e9ea15bf4
pranjalsk <pkaranka@asu.edu>
2017-10-29 19:53:37 -0700

    wallmart locations

--- Data_Hunting/Dataset_Places_Wallmart.py
+++ Data_Hunting/Dataset_Places_Wallmart.py
@@ -1,37 +1,25 @@
-# Source:Google Places API
+# Source:Wallmart API
 # API KEY: AIzaSyBs6Av8AeOqVL_cnEhxe7dmjtPqcFFrwM0
 
-#This code extracts the Walmart Stores Location details below using the Walmart API.
-
-import urllib2
-import json
+#Walmart Stores Location using the Walmart API.
 import csv
 import sys
+import urllib2
+import json
 reload(sys)
 sys.setdefaultencoding("utf8")
 
-apiKey = '86g35ew2ufsxjk33evj9vevk'
-city = 'tempe'
-url = 'http://api.walmartlabs.com/v1/stores?format=json&city=' + city + '&apiKey=' + apiKey
-json_obj = urllib2.urlopen(url)
-data_dict = json.load(json_obj)
+apiKey = 'fnx5k2fpwxjcmhh6ncrwtg2v'
+url = 'http://api.walmartlabs.com/v1/stores?format=json&city=' + 'tempe' + '&apiKey=' + apiKey
+dict_locations = json.load(urllib2.urlopen(url))
 
-csv_title = 'Walmart_Stores.csv'
-csv_location = 'CSV/' + csv_title
-csv_File = open(csv_location, 'w')
-csvwriter = csv.writer(csv_File)
-csvwriter.writerow(['Store', 'Store_Num', 'Store_name', 'Latitude', 'Longitude', 'Store_Address', 'Zip', 'Store_Phone_Num'])
+csv_file = open('CSV/Walmart_Stores.csv', 'w')
+csvwriter = csv.writer(csv_file)
+csvwriter.writerow(['Store', 'Store_name', 'Latitude', 'Longitude', 'Street_Address', 'Zip', 'Store_Phone_Num'])
 
-for store in data_dict:
-	Store_Num 	= store['no']
-	Store_name  = store['name']
-	Latitude    = store['coordinates'][1]
-	Longitude   = store['coordinates'][0]
-	Store_Address = store['streetAddress'] + ', ' +store['city'] + ', ' + store['stateProvCode']
-	#Store_City  = store['city']
-	Zip         = store['zip']
-	#State_Prov_Code = store['stateProvCode']
-	Store_Phone_Num = store['phoneNumber']
-	csvwriter.writerow(['Walmart',Store_Num, Store_name, Latitude, Longitude, Store_Address, Zip, Store_Phone_Num])	
-csv_File.close()
-print 'Done successfully'
+for store in dict_locations:
+	Street_Address = store['streetAddress'] + ', ' +store['city'] + ', ' + store['stateProvCode']
+	csvwriter.writerow(['Walmart', store['name'], store['coordinates'][1], store['coordinates'][0], Street_Address, store['zip'], store['phoneNumber']])	
+
+csv_file.close()
+print 'CSV Loaded'
